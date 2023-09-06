b5e93c343a491eba0ca7e985c05f0953566261ff
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-09-06 20:45:59 +0900

    [unrated] Title: 추억 점수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -4,9 +4,7 @@ function solution(name, yearning, photo) {
         let sum = 0;
         for (let j = 0; j < photo[i].length; j++) {
             let index = name.indexOf(photo[i][j]);
-            if (index === -1) {
-                continue;
-            } else {
+            if (index > -1) {
                 sum += yearning[index];
             }
         }
